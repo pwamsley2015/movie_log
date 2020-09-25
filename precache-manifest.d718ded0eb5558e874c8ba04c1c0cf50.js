bf528aef4fed551520041cbd953b3df5b4b27489
Patrick Wamsley <pwamsley2015@francisparker.org>
2020-09-25 15:46:20 -0700

    Updates

---
+++ precache-manifest.d718ded0eb5558e874c8ba04c1c0cf50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587a9bf98e1a660a298d7e503543c465",
+    "url": "/movie_log/index.html"
+  },
+  {
+    "revision": "bb94707b35d73d5ccb21",
+    "url": "/movie_log/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4298e75c98b0acc92a49",
+    "url": "/movie_log/static/css/main.ff1e280d.chunk.css"
+  },
+  {
+    "revision": "bb94707b35d73d5ccb21",
+    "url": "/movie_log/static/js/2.4fc3c4f6.chunk.js"
+  },
+  {
+    "revision": "fcca2cdcc5e6967026a1ba00172f2d47",
+    "url": "/movie_log/static/js/2.4fc3c4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4298e75c98b0acc92a49",
+    "url": "/movie_log/static/js/main.d3a28b02.chunk.js"
+  },
+  {
+    "revision": "58c3f117137347d9682f",
+    "url": "/movie_log/static/js/runtime-main.15efbcfa.js"
+  }
+]);
